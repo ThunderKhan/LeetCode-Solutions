5211796e7d543b4a1deda78a2ae184f308a3f280
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-11 20:57:06 +0530

    Solved 2235 in Java

---
+++ 02.Java/LC2235.java
@@ -0,0 +1,16 @@
+public class LC2235{
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int num1 = 5;
+        int num2 = 3;
+
+        System.out.println(sol.sum(num1, num2));
+    }
+}
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
