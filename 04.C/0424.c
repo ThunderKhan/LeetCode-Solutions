e9d14bc2ad73d67f0b88706cf14a9c3bb34e27d3
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-14 21:27:28 +0530

    Solved 0424 in C

---
+++ 04.C/0424.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+int characterReplacement(char* s, int k) {
+    int n = strlen(s);
+
+    int left = 0;
+    int maxFreq = 0;
+    int ans = 0;
+
+    int freq[26] = {0};
+
+    for (int right = 0; right < n; right++) {
+        int idx = s[right] - 'A';
+        freq[idx]++;
+
+        if (freq[idx] > maxFreq) {
+            maxFreq = freq[idx];
+        }
+
+        while (((right - left + 1) - maxFreq) > k) {
+            int idxL = s[left] - 'A';
+            freq[idxL]--;
+            left++;
+        }
+
+        int windowLen = right - left + 1;
+        if (windowLen > ans) {
+            ans = windowLen;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    char s[] = "ABAB";
+    int k = 2;
+
+    int result = characterReplacement(s, k);
+    printf("%d", result);
+
+    return 0;
+}
