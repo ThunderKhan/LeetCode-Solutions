2d5977520bcb606f18305f752ee77111afc56883
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-17 08:02:06 +0530

    Solved 2095 in C

---
+++ 04.C/2095.c
@@ -0,0 +1,23 @@
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* deleteMiddle(struct ListNode* head) {
+    if ((head == NULL) || (head->next == NULL)) {
+        return NULL;
+    }
+
+    struct ListNode *fast = head;
+    struct ListNode *slow = head;
+    struct ListNode *prev = NULL;
+
+    while (fast != NULL && fast->next != NULL) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    prev->next = slow->next;
+    return head;
+}
