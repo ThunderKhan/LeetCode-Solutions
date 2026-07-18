a8a726e733a47657f2df5315ce23c5826ae6d465
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-18 07:40:34 +0530

    Solved 0053 in Python

---
+++ 01.Python/0053.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        localSum = nums[0]
+        globalBestSum = nums[0]
+
+        for x in nums:
+            localSum = max(x, x + localSum)
+            globalBestSum = max(globalBestSum, localSum)
+        
+        return globalBestSum
+
+sol = Solution()
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(sol.maxSubArray(nums))
