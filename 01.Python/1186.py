d4b0fb6fb41f4e6f97cb5ec70217eb09b5bbef03
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-22 06:32:20 +0530

    Add Python solution for LeetCode 1186

---
+++ 01.Python/1186.py
@@ -0,0 +1,32 @@
+from typing import List
+
+class Solution:
+    def maximumSum(self, arr: List[int]) -> int:
+        noDelSum = arr[0]
+        oneDelSum = float('-inf')
+        result = arr[0]
+
+        for i in range(1, len(arr)):
+            prevNoDel = noDelSum
+
+            noDelSum = max(arr[i], arr[i] + noDelSum)
+            oneDelSum = max(prevNoDel, arr[i] + oneDelSum)
+
+            result = max(noDelSum, oneDelSum)
+        
+        return result
+
+sol = Solution()
+
+arr1 = [1, -2, 0, 3]
+result1 = sol.maximumSum(arr1)
+
+arr2 = [1, -2, -2, 3]
+result2 = sol.maximumSum(arr2)
+
+arr3 = [-1, -1, -1, -1]
+result3 = sol.maximumSum(arr3)
+
+print(result1)
+print(result2)
+print(result3)
