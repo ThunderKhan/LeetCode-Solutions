ca9d38ec3adfdc18225991bd6e423868355edec1
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-24 07:12:26 +0530

    Add Java solution for LeetCode 724

--- 02.Java/LC0724.java
+++ 02.Java/LC0724.java
@@ -7,12 +7,12 @@ public class LC0724 {
 class Solution {
     public int pivotIndex(int[] nums) {
         int totalSum = 0;
-        for (int i = 0; i < nums.lenght; i++) {
+        for (int i = 0; i < nums.length; i++) {
             totalSum += nums[i];
         }
         int left = 0;
 
-        for (int i = 0; i < nums.lenght; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int right = totalSum - left - nums[i];
 
             if (right == left) {
