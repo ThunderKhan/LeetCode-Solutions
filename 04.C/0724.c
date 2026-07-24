b0f5273190f1ca8a2613591393d8e69f09ece0d4
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-24 07:19:48 +0530

    Add C solution for LeetCode 724

---
+++ 04.C/0724.c
@@ -0,0 +1,19 @@
+int pivotIndex(int* nums, int numsSize) {
+    int totalSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        totalSum += nums[i];
+    }
+
+    int left = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int right = totalSum - left - nums[i];
+
+        if (right == left) {
+            return i;
+        }
+
+        left += nums[i];
+    }
+
+    return -1;
+}
