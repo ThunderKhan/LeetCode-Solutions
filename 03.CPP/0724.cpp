ee82b44f1ef80004c22feada09caa472657ebe89
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-24 07:15:54 +0530

    Add C++ solution for LeetCode 724

---
+++ 03.CPP/0724.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            totalSum += nums[i];
+        }
+
+        int left = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int right = totalSum - left - nums[i];
+
+            if (left == right) {
+                return i;
+            }
+
+            left += nums[i];
+        }
+
+        return -1;
+    }
+};
