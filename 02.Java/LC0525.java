a90d2b7380e931d3b292b40768a8245796f7256b
Ayan Khan <AyanKhanOffiiciall@gmail.com>
2026-07-31 09:22:52 +0530

    Add Java solution for LeetCode 525

---
+++ 02.Java/LC0525.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC0525 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {0, 1, 1, 1, 1, 1, 0, 0, 0};
+        int result = sol.findMaxLength(nums);
+
+        System.out.println(result);
+    }
+}
+
+class Solution {
+    int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int maxLen = 0;
+        int runningSum = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                runningSum -= 1;
+            } else {
+                runningSum += 1;
+            }
+
+            if (map.containsKey(runningSum)) {
+                int currLen = i - map.get(runningSum);
+                maxLen = Math.max(maxLen, currLen);
+            } else {
+                map.put(runningSum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
